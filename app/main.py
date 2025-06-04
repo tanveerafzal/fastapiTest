0010ef3c7940739ca5450e320824659cab7ff6dd
tanveerafzal <tanveerafzal@gmail.com>
2025-06-04 00:02:55 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from app.multiply_script import multiply
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello from FastAPI on Render!"}
+
+
+@router.get("/multiply")
+def multiply_route(a: int, b: int):
+    return {"result": multiply(a, b)}
